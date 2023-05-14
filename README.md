7a6f857ce67a12b0b83074909ad133b887e820b3
Nguyen Cong Anh Luan <conganhluan@gmail.com>
2023-05-15 02:05:50 +0700

    finish

---
+++ README.md
@@ -0,0 +1,51 @@
+# MESSAGE TO MAIL
+Đây là một chương trình phục vụ cho mục đích cá nhân chính là có thể chuyển những tin nhắn trên Bkel - trang web dùng để trao đổi của những giảng viên và sinh viên thuộc trường Đại học Bách Khoa - ĐHQG-HCM - thành những email và gửi về trực tiếp đến gmail cá nhân của mình.  
+Mã nguồn đã được viết theo hướng phù hợp với tất cả mọi người sử dụng nếu có bất kì ai cũng có nhu cầu giống mình; nhưng tất nhiên sẽ có vài điểm cần sửạ lại cho phù hợp với từng người, mình sẽ đề cập rõ hơn bên dưới.  
+  
+This is a program created for personal purpose, which is to convert the messages on Bkel - the website used for interactions between every teachers and students in HCM city University of Technology - into emails and send them directly to my personal email.  
+The source code is designed to be suitable for anyone whose need is like me; but of course, there will be some elements which need personalizing to fit the user, I will go into details below.  
+  
+## Điều kiện tiên quyết/ Presiquites:
+Để chạy được các code này, máy tính cần có Python. Ngoài các thư viện đã có sẵn, bạn cũng cần phải cài đặt những thư viện riêng mà chương trình đã sử dụng bao gồm `requests`, `emails`, `beautifulsoup4` nếu máy bạn chưa có. 
+Theo mặc định, chương trình sẽ sử dụng *localhost* cho SMTP (dịch vụ gửi thư), vậy nên máy tính của bạn cần phải được cài đặt dịch vụ SMTP trước, hoặc bạn có thể chọn lưu thành các tệp tin thay vì gửi mail để không cần phải cài đặt SMTP. Nếu bạn sử dụng dịch vụ SMTP của các trang web thì bạn phải mở khóa dòng 163 `s.login(SMTP_user, SMTP_password)` để có thể đăng nhập vào dịch vụ đó.  
+  
+To run the code, the computer needs Python. Beside in-built libraries, you also need to install specially needed libariries for the program such as `requests`, `emails`, `beautifulsoup4` if the computer doesn't have them yet.  
+By default, the program will use *localhost* for STMP (email service) so that your computer needs to install the SMTP in advance, unless you prefer saving messages into files instead of sending emails. If you use SMTP service on another host then you have to un-comment the code in line 163 `s.login(SMTP_user, SMTP_password)` which allows you to login those web services.  
+  
+## Cách cài đặt và sử dụng/ How to install and use:
+Để cài đặt, các bạn tải (hoặc nhân bản) mã nguồn về, sau đó điền đầy đủ các thông tin cá nhân vào tệp tin *personal_information.json* như sau:  
+- **email_sender**: Email dùng để gửi mail
+- **email_receiver**: Email dùng để nhận mail và thông báo cho bạn biết
+- **bkel_username**: Tên tài khoản dùng để đăng nhập vào *Dịch vụ xác thực tập trung của BKEL*
+- **bkel_password**: Mật khẩu của tài khoản dùng để đăng nhập vào *Dịch vụ xác thực tập trung của BKEL*
+- **webmail_host** : Máy chủ dùng để sử dụng dịch vụ SMTP (mặc định là *localhost*)
+- **SMTP_user**: Tên tài khoản dùng để đăng nhập vào dịch vụ SMTP, thường là **email_sender**
+- **SMTP_password**: Mật khẩu của tài khoản dùng để đăng nhập vào dịch vụ SMTP
+- **save_path**: Đường dẫn để thư mục lưu những tệp tin chứa nội dung tin nhắn. Ví dụ: "C:/Users/Desktop/", khi chạy thì chương trình sẽ tạo thư mục *Messages* trong đường dẫn đã thiết lập và tiến hành lưu các tệp tin vào đấy.
+- **use_option** : Bao gồm những lựa chọn sau đây:
+    - 1: Chuyển các tin nhắn thành mail và chuyển đến hộp thư điện tử như đã thiết lập
+    - 2: Chuyển các tin nhắn thành các tệp tin và lưu vào nơi đã được chỉ định
+    - 3: Thực hiện cả hai hành động trên
+  
+Để khởi chạy chương trình, bạn chỉ cần gọi lệnh `python /path/to/main.py` là hoàn thành. Để chương trình đạt được hiệu quả tốt nhất, bạn nên sử dụng một phần mềm khác để lên lịch thực hiện nó theo định kì. Nếu bạn chạy trên hosting của chính mình và gặp lỗi thì có thể mở khóa câu lệnh ở dòng 57 `#session.verify = False` để sửa lỗi. Mọi lỗi khác nếu có xin vui lòng liên hệ với mình để mình có thể sửa chữa một cách sớm nhất!  
+  
+To install, you download (or clone) the source code, then fill completely your personal settings into the file *personal_information.json* including:  
+- **email_sender**: The email used to send mails
+- **email_receiver**: The email used to receive mails and notify you
+- **bkel_username**: The account's username to log in the *Central Authentication Service for BKEL*
+- **bkel_password**: The account's password to log in the *Central Authentication Service for BKEL*
+- **webmail_host** :The host used to provide the SMTP service (mặc định là *localhost*)
+- **SMTP_user**: The email used to log in the SMTP service, normally **email_sender**
+- **SMTP_password**: The password along with email used to log in the SMTP service
+- **save_path**: The path to the directory storing messages. For example: "C:/Users/Desktop/"; when running, the program creates a directory *Messages* in the chosen path and save files in there.
+- **use_option** : There are options:
+    - 1: Convert the messages into mails and send them to the chosen email
+    - 2: Convert the messages into .html files and save them in the chosen path
+    - 3: Do both
+  
+To run the program, you will run `python /path/to/main.py` and it's done. For the most effciency, you should use another programs for scheduling this program to run periodically. If you run this program on your own hosting and encounter error, you may un-commment `#session.verify = False` to fix it. Please let me know when you encounter a bug, I will fix and update the program as soon as possible!  
+  
+## Nguồn tham khảo/ References
+Mình xin gửi lời cảm ơn chân thành đến những người lạ mặt mình chưa hề quen biết, nhưng lại giúp đỡ mình rất nhiều trên các diễn đàn, khi mình đang bước trên con đường của sự đau khổ và tuyệt vọng. Cụ thể hơn, những trang web mình đã học tập, tham khảo, cũng như tham gia vào các cuộc hỏi đáp hữu ích từ những người đi trước sẽ được mình để ở bài viết của mình [Mình đã xây dựng công cụ Mess To Mail như thế nào](https://conganhluan.com/personal-blog/hcmut-student/how-i-created-the-tool-mess-to-mail/(mở trong cửa sổ mới)), các bạn hãy đón xem nha!  
+  
+I would love to say sincerely "thank you" to those all strangers, who I never know, but have helped me a lot on the forums, when I am feeling depressed and painful. For more detail, the websites that I have learned, referenced, and taken part in extremely useful Q&As from predecessors will be revealed on the post [How I created the tool "Mess To Mail](https://conganhluan.com/personal-blog/hcmut-student/how-i-created-the-tool-mess-to-mail/(mở trong cửa sổ mới)), looking forward to seeing you there!
---
+++ main.py
@@ -0,0 +1,184 @@
+import os
+import sys
+import json
+import requests
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from bs4 import BeautifulSoup as BS
+
+#Functions used for debugging
+def printContent(string):
+    page = BS(string, "xml")
+    original_stdout = sys.stdout
+    with open("index.html","w") as file:
+        sys.stdout = file
+        print(page)
+        sys.stdout = original_stdout
+        file.close()
+
+def printJson(json_obj):
+    json_string = json.dumps(json_obj, indent=4)
+    file = open("response.json", "w")
+    file.write(json_string)
+    file.close()
+
+#Fucntion to get json object
+def getJson(path):
+    file = open(os.path.join(os.path.dirname(__file__), path), "r")
+    json_obj = json.load(file)
+    file.close()
+    return json_obj
+
+#Load the personal information
+information = getJson("personal_information.json")[0]
+bkel_username = information["bkel_username"]
+bkel_password = information["bkel_password"]
+email_sender = information["email_sender"]
+email_receiver = information["email_receiver"]
+webmail_host = information["webmail_host"]
+SMTP_user = information["SMTP_user"]
+SMTP_password = information["SMTP_password"]
+save_path = information["save_path"]
+use_option = information["use_option"]
+
+#Pre-check for the program
+if (use_option!=1 and use_option!=2 and use_option!=3):
+    print("The option number is wrong, please read README.md and modify the personal_information.json properly!")
+    quit()
+
+#URL for authentication
+url1 = "https://sso.hcmut.edu.vn/cas/login?service=https://e-learning.hcmut.edu.vn/login/index.php?authCAS=CAS"
+#URL for getting sesskey and userid
+url2 = "https://e-learning.hcmut.edu.vn/message/index.php"
+
+#Start the session
+session = requests.Session()
+#session.verify = False                                                         #if there is error about SSLAuthentication, un-commment this line
+
+#Phase 1 - Get the needed cookies for the authentication pages
+
+response = session.get(url1)
+
+data = {
+    "username" : bkel_username,
+    "password" : bkel_password
+}
+finder = BS(response.content,"lxml")
+foundList = finder.find_all("input")
+for i in range(3, 7):                                                           #if the web is updated, this code (range(3,7)) may create bugs
+    hiddenData = foundList[i]
+    data[hiddenData.attrs["name"]] = hiddenData.attrs["value"]
+
+#Phase 2 - Bypass the authentication with username and password (and hidden tickets, etc.)
+
+response = session.post(url1, data=data)
+
+    #Navigate to messages page
+response = session.get(url2)
+
+    #Get the sesskey for the url
+finder = BS(response.content,"lxml")
+json_url = finder.find_all("a")[22].attrs["href"].replace("https://e-learning.hcmut.edu.vn/login/logout.php?sesskey=", "https://e-learning.hcmut.edu.vn/lib/ajax/service.php?sesskey=") + "&info=core_message_get_conversation_counts,core_message_get_unread_conversation_counts"
+                                                                                #if the web is updated, this code (22) may create bugs
+    #Get the userid for the data
+userid = finder.find_all("div")[12].attrs["data-userid"]                        #if the web is updated, this code (12) may create bugs
+
+    #Load the json prepared for the request
+json_obj = getJson("get_unread_chats_count.json")
+json_obj[0]["args"]["userid"] = userid
+json_obj[1]["args"]["userid"] = userid
+
+    #Check if there are unread messages
+response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
+json_response = response.json()
+num_of_unread_chat = json_response[1]["data"]["types"]["1"]
+if num_of_unread_chat == 0:
+    sys.exit("There is none of unread messages")
+else:
+    print("There are " + str(num_of_unread_chat) + " unread chats")
+
+#Phase 3 - Get the unread messages
+
+    #Load the json prepared for the request
+json_obj = getJson("get_unread_chats.json")
+json_obj[0]["args"]["userid"] = userid
+
+    #Update the url and get id, and number of unread messages of the unread chats
+json_url = json_url.replace("core_message_get_conversation_counts,core_message_get_unread_conversation_counts", "core_message_get_conversations")
+response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
+json_response = response.json()
+
+id_unread_chats = []
+num_unread_messages = []
+messages = {}
+for i in range(0, num_of_unread_chat):
+    chat = json_response[0]["data"]["conversations"][i]
+    id_unread_chats.append(chat["id"])
+    num_unread_messages.append(chat["unreadcount"])
+
+    #Load the json prepared for the request
+json_obj = getJson("get_messages.json")
+json_obj[0]["args"]["currentuserid"] = int(userid)
+
+    #Update url and get unread messages
+json_url = json_url.replace("core_message_get_conversations", "core_message_get_conversation_messages")
+
+    #For each unread chat
+for i in range(0, num_of_unread_chat):
+    json_obj[0]["args"]["convid"] = int(id_unread_chats[i])
+    json_obj[0]["args"]["limitnum"] = int(num_unread_messages[i])
+    response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
+    json_response = response.json()
+    author = json_response[0]["data"]["members"][0]["fullname"]
+    messages[author] = []
+
+        #For each unread messages in that chat
+    for j in range(0, num_unread_messages[i]):
+        message = json_response[0]["data"]["messages"][j]["text"]
+        messages[author].append(message)
+
+#Phase 4 - Mark messages all read and log out
+'''
+    #Load the json prepared for the request
+json_obj = getJson("mark_messages_read.json")
+json_obj[0]["args"]["userid"] = int(userid)
+
+    #Update url and mark all messages as read
+json_url = json_url.replace("core_message_get_conversation_messages", "core_message_mark_all_conversation_messages_as_read")
+for i in range(0, num_of_unread_chat):
+    json_obj[0]["args"]["conversationid"] = int(id_unread_chats[i])
+    response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
+    json_response = response.json()
+'''
+session.close()
+
+#Phase 5 - Send the emails/ Save into the files
+
+    #Send the emails
+if use_option == 1 or use_option == 3:
+    for i in messages:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = i + " has sent from the Bkel"
+        msg['From'] = email_sender
+        msg['To'] = email_receiver
+        for j in messages[i]:
+            body = MIMEText(j, 'html')
+            msg.attach(body)
+            s = smtplib.SMTP(webmail_host)
+            #s.login(SMTP_user, SMTP_password)
+            s.sendmail(email_sender, email_receiver, msg.as_string())
+            s.quit()
+
+    #Save into the files
+if use_option == 2 or use_option == 3:
+    save_path = save_path + "Messages"
+    if os.path.exists(save_path) == False:
+        os.mkdir(save_path)
+
+    for i in messages:
+        for j in messages[i]:
+            file = open(os.path.join(save_path, i + ".html"), mode="a", encoding="UTF-8")
+            file.write(j)
+            file.close()
+
