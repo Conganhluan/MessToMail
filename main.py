7c56ee5e6a5ef2822c164bce31031fcce94632fd
Nguyen Cong Anh Luan <conganhluan@gmail.com>
2023-05-22 21:12:45 +0700

    add "log out" when job is done

--- main.py
+++ main.py
@@ -61,17 +61,17 @@ session = requests.Session()
 
 #Phase 1 - Get the needed cookies for the authentication pages
 
-for retry in range(MAX_RETRIES): 
+for retry in range(0, MAX_RETRIES): 
     response = session.get(url1)
     if response.ok:
         break
     else:
-        print("Connect failed with the url!" + response.url)
+        print("Connect failed with the url!" + url1)
         attempts_left = MAX_RETRIES - retry - 1
         if (attempts_left != 0):
             print(str(attempts_left) + " attempts left!")
         else: 
-            print("Stop the program!")
+            print("Quit the program!")
             quit()
 
 data = {
@@ -86,36 +86,37 @@ for i in range(3, 7):                                                           
 
 #Phase 2 - Bypass the authentication with username and password (and hidden tickets, etc.)
 
-for retry in range(MAX_RETRIES):
+for retry in range(0, MAX_RETRIES):
     response = session.post(url1, data=data)
     if response.ok:
         break
     else:
-        print("Connect failed with the url!" + response.url)
+        print("Connect failed with the url!" + url1)
         attempts_left = MAX_RETRIES - retry - 1
         if (attempts_left != 0):
             print(str(attempts_left) + " attempts left!")
         else: 
-            print("Stop the program!")
+            print("Quit the program!")
             quit()
 
     #Navigate to messages page
-for retry in range(MAX_RETRIES):
+for retry in range(0, MAX_RETRIES):
     response = session.get(url2)
     if response.ok:
         break
     else:
-        print("Connect failed with the url!" + response.url)
+        print("Connect failed with the url!" + url2)
         attempts_left = MAX_RETRIES - retry - 1
         if (attempts_left != 0):
             print(str(attempts_left) + " attempts left!")
         else: 
-            print("Stop the program!")
+            print("Quit the program!")
             quit()
 
     #Get the sesskey for the url
 finder = BS(response.content,"lxml")
-json_url = finder.find_all("a")[22].attrs["href"].replace("https://e-learning.hcmut.edu.vn/login/logout.php?sesskey=", "https://e-learning.hcmut.edu.vn/lib/ajax/service.php?sesskey=") + "&info=core_message_get_conversation_counts,core_message_get_unread_conversation_counts"
+url_logout = finder.find_all("a")[22].attrs["href"]
+json_url = url_logout.replace("https://e-learning.hcmut.edu.vn/login/logout.php?sesskey=", "https://e-learning.hcmut.edu.vn/lib/ajax/service.php?sesskey=") + "&info=core_message_get_conversation_counts,core_message_get_unread_conversation_counts"
                                                                                 #if the web is updated, this code (22) may create bugs
     #Get the userid for the data
 userid = finder.find_all("div")[12].attrs["data-userid"]                        #if the web is updated, this code (12) may create bugs
@@ -126,17 +127,17 @@ json_obj[0]["args"]["userid"] = userid
 json_obj[1]["args"]["userid"] = userid
 
     #Check if there are unread messages
-for retry in range(MAX_RETRIES):
+for retry in range(0, MAX_RETRIES):
     response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
     if response.ok:
         break
     else:
-        print("Connect failed with the url!" + response.url)
+        print("Connect failed with the url!" + json_url)
         attempts_left = MAX_RETRIES - retry - 1
         if (attempts_left != 0):
             print((attempts_left) + " attempts left!")
         else: 
-            print("Stop the program!")
+            print("Quit the program")
             quit()
 
 json_response = response.json()
@@ -157,17 +158,17 @@ json_obj[0]["args"]["userid"] = userid
     #Update the url and get id, and number of unread messages of the unread chats
 json_url = json_url.replace("core_message_get_conversation_counts,core_message_get_unread_conversation_counts", "core_message_get_conversations")
 
-for retry in range(MAX_RETRIES):
+for retry in range(0, MAX_RETRIES):
     response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
     if response.ok:
         break
     else:
-        print("Connect failed with the url!" + response.url)
+        print("Connect failed with the url!" + json_url)
         attempts_left = MAX_RETRIES - retry - 1
         if (attempts_left != 0):
             print(str(attempts_left) + " attempts left!")
         else: 
-            print("Stop the program!")
+            print("Quit the program")
             quit()
 
 json_response = response.json()
@@ -193,18 +194,18 @@ for i in range(0, num_of_unread_chat):
     json_obj[0]["args"]["convid"] = int(id_unread_chats[i])
     json_obj[0]["args"]["limitnum"] = int(num_unread_messages[i])
     
-    for retry in range(MAX_RETRIES):
+    for retry in range(0, MAX_RETRIES):
         response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
         if response.ok:
             break
         else:
-            print("Connect failed with the url!" + response.url)
+            print("Connect failed with the url!" + json_url)
             attempts_left = MAX_RETRIES - retry - 1
             if (attempts_left != 0):
                 print(str(attempts_left) + " attempts left!")
             else: 
-                print("Stop the program!")
-                quit()
+                print("Pass over the url " + json_url)
+                break
     
     json_response = response.json()
     author = json_response[0]["data"]["members"][0]["fullname"]
@@ -226,21 +227,35 @@ json_url = json_url.replace("core_message_get_conversation_messages", "core_mess
 for i in range(0, num_of_unread_chat):
     json_obj[0]["args"]["conversationid"] = int(id_unread_chats[i])
     
-    for retry in range(MAX_RETRIES):
+    for retry in range(0, MAX_RETRIES):
         response = session.post(json_url, json=json_obj, headers={"Accept": "application/json"})
         if response.ok:
             break
         else:
-            print("Connect failed with the url!" + response.url)
+            print("Connect failed with the url " + json_url)
             attempts_left = MAX_RETRIES - retry - 1
             if (attempts_left != 0):
                 print(str(attempts_left) + " attempts left!")
             else: 
-                print("Stop the program!")
-                quit()
+                print("Pass over the url " + json_url)
+                break
     
     json_response = response.json()
 '''
+
+for retry in range(0, MAX_RETRIES):
+    response = session.get(url_logout)
+    if response.ok:
+        break
+    else:
+        print("Log out fail!")
+        attempts_left = MAX_RETRIES - retry - 1
+        if (attempts_left != 0):
+            print(str(attempts_left) + " attempts left!")
+        else: 
+            print("Pass over the log out url!")
+            break
+
 session.close()
 
 #Phase 5 - Send the emails/ Save into the files
